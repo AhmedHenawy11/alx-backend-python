3ce2484346f1a28765d57fc33542f1275509407f
AhmedHenawy11 <ahmedhenawy1511@gmail.com>
2024-10-12 05:56:21 +0300

    task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""
+a type-annotated function concat that takes a string str1 and a string
+ str2 as arguments and returns a concatenated string
+"""
+
+def conacat(str1: str, str2: str):
+    return str1 + str2
