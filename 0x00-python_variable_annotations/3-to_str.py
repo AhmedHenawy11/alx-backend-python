8cfda4ef481aa3ed501b382d060336ceb8559363
AhmedHenawy11 <ahmedhenawy1511@gmail.com>
2024-10-12 20:08:28 +0300

    fix 3 style

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -4,6 +4,7 @@ that takes a float n as argument and
 returns the string representation of the float.
 """
 
-def to_str(n:float) -> str :
+
+def to_str(n: float) -> str:
     """Returns the string of the float"""
     return str(n)
