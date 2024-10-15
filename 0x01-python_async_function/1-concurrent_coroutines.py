87e43d3a1811ab47bac903d0d17968efb46832f0
AhmedHenawy11 <ahmedhenawy1511@gmail.com>
2024-10-15 05:56:16 +0300

    task 1 fix

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,22 +1,27 @@
 #!/usr/bin/env python3
-"""Take the code from wait_n and alter it into a new function
-task_wait_n. The code is nearly identical to wait_n except
-task_wait_random is being called.
+"""Import wait_random from the previous python file that you’ve
+written and write an async routine called wait_n that takes in 2
+int arguments (in this order): n and max_delay. You will spawn
+wait_random n times with the specified max_delay.
+
+wait_n should return the list of all the delays (float values).
+The list of the delays should be in ascending order without using
+sort() because of concurrency.
 """
 
 
 import asyncio
 from typing import List
-task_wait_random = __import__('3-tasks').task_wait_random
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> List[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """Spawn wait_random n times"""
     tasks = []
     delays = []
 
     for i in range(n):
-        task = task_wait_random(max_delay)
+        task = wait_random(max_delay)
         tasks.append(task)
 
     for task in asyncio.as_completed((tasks)):
